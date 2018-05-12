7cb6a9638748116de96c95c1745b7314379f88b8
ProstoNiko777 <n_stebletsky@mail.ru>
2018-05-12 15:25:01 +0300

    lesson2 start

--- src/by/it/stebletsky/lesson02/TaskA2.java
+++ src/by/it/stebletsky/lesson02/TaskA2.java
@@ -9,4 +9,7 @@ package by.it.stebletsky.lesson02;
 */
 class TaskA2 {
 
+    public static void main(String[] args) {
+        for ((int i=0; i < 5; i++))
+        System.out.println("Я начинаю изучать Java!");
 }
