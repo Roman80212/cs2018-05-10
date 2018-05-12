631bb79c6ca849196117ef4b2e1ea1e60703655c
vkalenik <uladzimirkalenik@gmail.com>
2018-05-12 15:33:45 +0300

    lesson2 A3

--- src/by/it/vkalenik/lesson02/TaskA2.java
+++ src/by/it/vkalenik/lesson02/TaskA2.java
@@ -10,7 +10,7 @@ package by.it.vkalenik.lesson02;
 class TaskA2 {
     public static void main(String[] args) {
         for (int i = 0; i < 5; i++) {
-            System.out.println("я начинаю изучать Java!");
+            System.out.println("Я начинаю изучать Java!");
         }
     }
 }
