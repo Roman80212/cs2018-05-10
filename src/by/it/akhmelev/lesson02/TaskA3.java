57f86da6fdc31c6a9de69f6d4fd52c1f358c9780
DzianisMatveyenka <dzianis.matveyenka@mail.ru>
2018-05-12 15:42:35 +0300

    lesson2 start

--- src/by/it/akhmelev/lesson02/TaskA3.java
+++ src/by/it/akhmelev/lesson02/TaskA3.java
@@ -9,7 +9,6 @@ class TaskA3 {
     public static void main(String[] args) {
         int i = 3;
         int j = 4;
-        int y;
         int k = i * i + j * j;
         System.out.println(i + "*" + i + "+" + j + "*" + j + "=" + k);
     }
