1810f9fc73d6f2882f1892d095b371dc0ef84489
lepchenkov <k.lepchenkov@gmail.com>
2018-05-13 08:46:53 +0300

    test tasks

--- src/by/it/lepchenkov/lesson02/TaskB1.java
+++ src/by/it/lepchenkov/lesson02/TaskB1.java
@@ -16,5 +16,12 @@ Scanner sc=new Scanner(System.in);
 
  */
 
+import java.util.Scanner;
+
 class TaskB1 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int rollno=sc.nextInt();
+        System.out.println(rollno*rollno);
+    }
 }
