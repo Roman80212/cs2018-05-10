6f6ccb9a824b1b25385f57b69f5678ee516c6d2b
Ксения <ksu.litvin.000@mail.ru>
2018-05-13 15:23:05 +0300

    lesson2 start

--- src/by/it/klitvin/lesson02/TaskB1.java
+++ src/by/it/klitvin/lesson02/TaskB1.java
@@ -16,5 +16,14 @@ Scanner sc=new Scanner(System.in);
 
  */
 
+import java.util.Scanner;
+
 class TaskB1 {
+    public static void main(String[] args) {
+        System.out.println("Введите любое число:");
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int i = a*a;
+        System.out.println("Квадрат данного числа" + "=" + i);
+    }
 }
