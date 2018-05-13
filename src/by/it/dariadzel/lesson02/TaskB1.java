d3a3684a63b6fea54d711dcb92bacd41da4664d4
dariadzel <dariadzel@gmail.com>
2018-05-13 15:06:22 +0300

    lesson2 start

--- src/by/it/dariadzel/lesson02/TaskB1.java
+++ src/by/it/dariadzel/lesson02/TaskB1.java
@@ -20,8 +20,8 @@ import java.util.Scanner;
 
 class TaskB1 {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         int i = sc.nextInt();
-        System.out.println(i*i);
+        System.out.println(i * i);
     }
 }
