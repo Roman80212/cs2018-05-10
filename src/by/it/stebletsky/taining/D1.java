380ae516444fd48b654970ac68fd94a16bf24862
ProstoNiko777 <n_stebletsky@mail.ru>
2018-05-14 19:04:33 +0300

    lesson2 15 May

---
+++ src/by/it/stebletsky/taining/D1.java
@@ -0,0 +1,18 @@
+package by.it.stebletsky.taining;
+
+
+
+public class D1 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Ввод");
+double a = 3.89;
+        System.out.println("Дробная часть");
+        System.out.println(String.format("%.2g%n", a));
+
+
+
+
+    }
+}
