2c708cab7af0ca7b25706fe859747c46512fb6bc
Roman80212 <fonarik33@gmail.com>
2018-05-14 23:17:01 +0300

    lesson02

--- src/by/it/Yrkevich/lesson02/TaskC1.java
+++ src/by/it/Yrkevich/lesson02/TaskC1.java
@@ -1,5 +1,7 @@
 package by.it.Yrkevich.lesson02;
 
+import java.util.Scanner;
+
 /* Нужно написать программу, которая вводит два числа с клавиатуры
 и выводит их сумму на экран в виде
 
@@ -10,9 +12,16 @@ package by.it.Yrkevich.lesson02;
 Sum = 26
 */
 class TaskC1 {
-
-
-
-
-
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int i=sc.nextInt();
+        int x=sc.nextInt();
+        System.out.println("Ввод:");
+        System.out.print(i);
+        System.out.print(" "+x);
+        System.out.println();
+        System.out.println("Вывод");
+        int z=i+x;
+        System.out.println("Sum" + " = "+z);
+    }
 }
--- src/by/it/Yrkevich/lesson02/TaskC2.java
+++ src/by/it/Yrkevich/lesson02/TaskC2.java
@@ -1,5 +1,7 @@
 package by.it.Yrkevich.lesson02;
 
+import java.util.Scanner;
+
 /* Нужно написать программу, которая вводит два числа с клавиатуры
 и 4 раза выводит их сумму с обозначением системы счисления на экран в
 ДЕСЯТИЧНОМ ДВОИЧНОМ ШЕСТНАДЦАТИРИЧНОМ ВОСЬМИРИЧНОМ виде
@@ -14,6 +16,32 @@ HEX:22+1a=3c
 OCT:42+32=74
 */
 class TaskC2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+        int x = sc.nextInt();
+        int z = i + x;
+        System.out.println("DEC:" + i + "+" + x + "=" + z);
+        //двоичная
+        Integer number = i;
+        String q = Integer.toBinaryString(number);
 
+        Integer n = x;
+        String w = Integer.toBinaryString(n);
+
+        System.out.println("BIN:" + w +"+" +q+ "="+ w+q);
+//16
+
+        q = Integer.toHexString(number).toUpperCase();
+        System.out.println(q);
+        w = Integer.toHexString(n).toUpperCase();
+        System.out.println(w);
+ //8
+        q = Integer.toOctalString(number);
+        System.out.println(q);
+        w = Integer.toOctalString(n);
+        System.out.println(w);
+
+    }
 
 }
