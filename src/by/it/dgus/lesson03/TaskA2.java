c452016d3a12edb27292bded650dbc90d85a85ce
dgus <dgus@tut.by>
2018-05-15 21:08:59 +0300

    lesson3 start

--- src/by/it/dgus/lesson03/TaskA2.java
+++ src/by/it/dgus/lesson03/TaskA2.java
@@ -1,5 +1,7 @@
 package by.it.dgus.lesson03;
 
+import java.util.Scanner;
+
 /*
 Lesson 03. Task A2. Стихотворение
 
@@ -21,5 +23,14 @@ Lesson 03. Task A2. Стихотворение
 Для автоматической проверки откройте и запустите класс Testing
 */
 class TaskA2 {
+    public static void main(String[] args) {
+        System.out.println("Мое любимое стихотворение:");
+        String a="У лукоморья дуб зелёный;";
+        String b= "Златая цепь на дубе том:";
+        String c= "И днём и ночью кот учёный";
+        String d="Все ходит по цепи кругом;";
+        System.out.println(a+"\n"+b+"\n"+c+"\n"+d);
 
+
+    }
 }
