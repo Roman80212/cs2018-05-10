094a4fd6c257a672bd85eda40d52ffb7545c0e2f
DzianisMatveyenka <dzianis.matveyenka@mail.ru>
2018-05-15 21:37:26 +0300

    lesson3 ok

--- src/by/it/akhmelev/lesson02/TaskC1.java
+++ src/by/it/akhmelev/lesson02/TaskC1.java
@@ -1,5 +1,7 @@
 package by.it.akhmelev.lesson02;
 
+import java.util.Scanner;
+
 /* Нужно написать программу, которая вводит два числа с клавиатуры
 и выводит их сумму на экран в виде
 
@@ -10,9 +12,13 @@ package by.it.akhmelev.lesson02;
 Sum = 26
 */
 class TaskC1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
+        int j = scan.nextInt();
+        System.out.println("Sum = " + (i + j));
 
-
-
+    }
 
 
 }
--- src/by/it/akhmelev/lesson02/TaskC2.java
+++ src/by/it/akhmelev/lesson02/TaskC2.java
@@ -1,5 +1,7 @@
 package by.it.akhmelev.lesson02;
 
+import java.util.Scanner;
+
 /* Нужно написать программу, которая вводит два числа с клавиатуры
 и 4 раза выводит их сумму с обозначением системы счисления на экран в
 ДЕСЯТИЧНОМ ДВОИЧНОМ ШЕСТНАДЦАТИРИЧНОМ ВОСЬМИРИЧНОМ виде
@@ -14,6 +16,23 @@ HEX:22+1a=3c
 OCT:42+32=74
 */
 class TaskC2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
+        int j = scan.nextInt();
 
-
+        System.out.println("DEC:"+i+"+"+j+"="+(i+j));
+        String bi=Integer.toBinaryString(i);
+        String bj=Integer.toBinaryString(j);
+        String bs=Integer.toBinaryString(i+j);
+        System.out.println("BIN:"+bi+"+"+bj+"="+bs);
+        String hi=Integer.toHexString(i);
+        String hj=Integer.toHexString(j);
+        String hs=Integer.toHexString(i+j);
+        System.out.println("HEX:"+hi+"+"+hj+"="+hs);
+        String oi=Integer.toOctalString(i);
+        String oj=Integer.toOctalString(j);
+        String os=Integer.toOctalString(i+j);
+        System.out.println("OCT:"+oi+"+"+oj+"="+os);
+    }
 }
