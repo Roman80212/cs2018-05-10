06f82fe711a3b6e1cc0aa69cd2085a8bbf32f5e8
ProstoNiko777 <n_stebletsky@mail.ru>
2018-05-16 16:03:44 +0300

    lesson3 16May

--- src/by/it/stebletsky/taining/T2m16.java
+++ src/by/it/stebletsky/taining/T2m16.java
@@ -2,7 +2,12 @@ package by.it.stebletsky.taining;
 
 public class T2m16 {
     public static void main(String[] args) {
-        if (3>2) System.out.println("все ок!");
+        double x, y, z;
+        x = 3;
+        y = 4;
+        z = Math.sqrt(x * x + y * y);
+        System.out.println("Длина гиппотинузы: " +z);
+
     }
 
 }
