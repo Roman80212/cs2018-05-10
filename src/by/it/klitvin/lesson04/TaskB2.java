3aa4b5a0b66f1fe731cc9a592c01ca255bb0a823
klitvin <ksu.litvin.000@mail.ru>
2018-05-18 21:31:37 +0300

    1

--- src/by/it/klitvin/lesson04/TaskB2.java
+++ src/by/it/klitvin/lesson04/TaskB2.java
@@ -1,5 +1,7 @@
 package by.it.klitvin.lesson04;
 
+import java.util.Scanner;
+
 /*
 Таблица умножения
 Вывести на экран таблицу умножения size х size используя цикл while.
@@ -24,5 +26,27 @@ size прочитать c клавиатуры. Числа разделить п
 3. Программа должна выводить таблицу умножения size х size.
  */
 public class TaskB2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int size = scan.nextInt();
+        int a= 1;
+        while (size>0)   {
+           while(a!=0)
+            System.out.print(size+" ");
 
+            n++;
+
+            System.out.print(size*n+" ");
+
+        }
+
+        size++;
+
+        n=1;
+
+        System.out.println("");
+
+
+    }
 }
+
