c3a96ba6d07da19fc140d0b4788276a13a61dc39
dariadzel <dariadzel@gmail.com>
2018-05-19 00:49:14 +0300

    lesson4 start

--- src/by/it/dariadzel/lesson04/TaskB3.java
+++ src/by/it/dariadzel/lesson04/TaskB3.java
@@ -25,7 +25,46 @@ package by.it.dariadzel.lesson04;
  */
 
 
+import java.util.Scanner;
 
 public class TaskB3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
 
+
+        int i;
+        int sum = 0;
+
+        for (i = 0; i < 1000; i++) {
+            sum += i;
+            if (sum == 0) {
+                System.out.print("");
+
+            } else {
+
+                if (sum > a) break;
+                System.out.print(sum + ",");
+            }
+
+
+        }
+
+
+    }
 }
+
+  /*  while (sum < a) {
+            int i = 1;
+            sum += i;
+            System.out.print(sum + ",");
+            ++i;
+
+
+       /* for ( int i = 0; i > 10 ; i ++){
+
+            int sum = i + i++;
+            System.out.print(sum + ",");
+           // i++;
+            //if(sum >= a) break;*/
+
--- src/by/it/dariadzel/lesson04/TaskC1.java
+++ src/by/it/dariadzel/lesson04/TaskC1.java
@@ -1,5 +1,7 @@
 package by.it.dariadzel.lesson04;
 
+import java.util.Scanner;
+
 /*
 Напишите программу которая спрашивает у пользователя:
 Какую вы хотите зарплату в $$$?
@@ -37,6 +39,28 @@ package by.it.dariadzel.lesson04;
 
 */
 public class TaskC1 {
+    public static void main(String[] args) {
+        System.out.println("Какую вы хотите зарплату в $$$?");
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+
+        if (a < 300 || a > 3000){
+            System.out.println("Мы вам перезвоним!");
+            }
+        else if (300 >= a && a <= 3000){
+            for (int month = 0; month <15; month++){
+                if (month<7){
+                    switch (month){
+                        case 0: System.out.println("За месяц 0 начислено $" + month);
+                    }
+                }
+
+            }
+
+
+        }
+    }
 
 
 }
