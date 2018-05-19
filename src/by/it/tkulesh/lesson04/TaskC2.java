a51b67556fb68b8dc5c98f32c79c2818a32c3caf
Kulesh <tati.kulesh@gmail.com>
2018-05-19 10:18:38 +0300

    lesson4 start

--- src/by/it/tkulesh/lesson04/TaskC2.java
+++ src/by/it/tkulesh/lesson04/TaskC2.java
@@ -1,5 +1,7 @@
 package by.it.tkulesh.lesson04;
 
+import java.util.Scanner;
+
 /*
 
 На входе вводится число и месяц 2018 года.
@@ -27,5 +29,61 @@ package by.it.tkulesh.lesson04;
 
  */
 public class TaskC2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int d = scan.nextInt();
+        int m = scan.nextInt();
+        int numb;
+        if (m == 1) {
+            numb = d;
+            System.out.println(numb);
+        }
+        if (m == 2) {
+            numb = 31 + d;
+            System.out.println(numb);
+        }
+        if (m == 3) {
+            numb = 31 + 28 + d;
+            System.out.println(numb);
+        }
+        if (m == 4) {
+            numb = 31 + 28 + 31 + d;
+            System.out.println(numb);
+        }
+        if (m == 5) {
+            numb = 31 + 28 + 31 + 30 + d;
+            System.out.println(numb);
+        }
+        if (m == 6) {
+            numb = 31 + 28 + 31 + 30 + 31 + d;
+            System.out.println(numb);
+        }
+        if (m == 7) {
+            numb = 31 + 28 + 31 + 30 + 31 + 30 + d;
+            System.out.println(numb);
+        }
+        if (m == 8) {
+            numb = 31 + 28 + 31 + 30 + 31 + 30 + 31 + d;
+            System.out.println(numb);
+        }
+        if (m == 9) {
+            numb = 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + d;
+            System.out.println(numb);
+        }
+        if (m == 10) {
+            numb = 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + d;
+            System.out.println(numb);
+        }
+        if (m == 11) {
+            numb = 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + d;
+            System.out.println(numb);
+        }
+        if (m == 12) {
+            numb = 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + 30 + d;
+            System.out.println(numb);
+        }
+
+
+    }
 
 }
