b61087d9694e6a056dd5899059828cd8a28f48e0
lepchenkov <k.lepchenkov@gmail.com>
2018-05-19 08:01:49 +0300

    test tasks

--- src/by/it/lepchenkov/lesson04/TaskC3.java
+++ src/by/it/lepchenkov/lesson04/TaskC3.java
@@ -24,5 +24,101 @@ package by.it.lepchenkov.lesson04;
 */
 
 public class TaskC3 {
+    static String numberName(int x) {
+        String numberN;
+        switch (x) {
+            case 1:
+                numberN = "один";
+                break;
+            case 2:
+                numberN = "два";
+                break;
+            case 3:
+                numberN = "три";
+                break;
+            case 4:
+                numberN = "четыре";
+                break;
+            case 5:
+                numberN = "пять";
+                break;
+            case 6:
+                numberN = "шесть";
+                break;
+            case 7:
+                numberN = "семь";
+                break;
+            case 8:
+                numberN = "восемь";
+                break;
+            case 9:
+                numberN = "девять";
+                break;
+            case 10:
+                numberN = "десять";
+                break;
+            case 11:
+                numberN = "одиннадцать";
+                break;
+            case 12:
+                numberN = "двенадцать";
+                break;
+            case 13:
+                numberN = "триннадцать";
+                break;
+            case 14:
+                numberN = "четырнадцать";
+                break;
+            case 15:
+                numberN = "пятадцать";
+                break;
+            case 16:
+                numberN = "шестнадцать";
+                break;
+            case 17:
+                numberN = "семнадцать";
+                break;
+            case 18:
+                numberN = "восемнадцать";
+                break;
+            case 19:
+                numberN = "девятнадцать";
+            case 20:
+                numberN = "девятнадцать";
+            case 30:
+                numberN = "девятнадцать";
+            case 40:
+                numberN = "девятнадцать";
+            case 50:
+                numberN = "девятнадцать";
+            case 60:
+                numberN = "девятнадцать";
+            case 70:
+                numberN = "девятнадцать";
+            case 80:
+                numberN = "девятнадцать";
+            case 90:
+                numberN = "девятнадцать";
+            case 100:
+                numberN = "девятнадцать";
+                break;
+            default:
+                numberN = "такого вариант нет";
+        }
+        return numberN;
+    }
+
+    public static void main(String[] args) {
+        int size = 10;
+        int j = 2;
+        while (j <= size) {
+            int i = 2;
+            while (i <= size) {
+                System.out.println(TaskC3.numberName(j) + " умножить на " + TaskC3.numberName(i) + " равно "+ j*i);
+                i++;
+            }
+            j++;
+        }
+    }
 
 }
