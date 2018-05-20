f8e601c508c26e28c94c0c41f298d252d99bc4b0
klitvin <ksu.litvin.000@mail.ru>
2018-05-20 14:38:56 +0300

    1

--- src/by/it/klitvin/lesson05/TaskC1.java
+++ src/by/it/klitvin/lesson05/TaskC1.java
@@ -10,18 +10,46 @@ package by.it.klitvin.lesson05;
     Сначала тот, который для x%3, потом тот, который для x%2, потом последний.
 */
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class TaskC1 {
-    public static void main(String[] args) throws Exception
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        ArrayList<Integer> lista = new ArrayList<Integer>();
+        ArrayList<Integer> listb = new ArrayList<Integer>();
+        ArrayList<Integer> listc = new ArrayList<Integer>();
+
+        for (int i = 0; i < 20; i++){
+            list.add(Integer.parseInt(reader.readLine()));
+        }
+        for (Integer i: list)
+        {
+            if (i % 3 == 0 && i % 2 == 0){
+                lista.add(i);
+                listb.add(i);
+            }
+            else if (i % 3 == 0)
+                lista.add(i);
+            else if (i % 2 == 0)
+                listb.add(i);
+            else
+                listc.add(i);
+        }
+        printList(lista);
+        printList(listb);
+        printList(listc);
+    }
+
+    public static void printList(List<Integer> list)
     {
+        for (Integer i : list){
+            System.out.println(i);
 
-
+        }
     }
-
-    private static void printList(List<Integer> list) {
-        for (Integer aList : list) System.out.println(aList);
-    }
-
-
 }
