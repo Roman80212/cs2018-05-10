31746e0ab6c43446ff83944f742744b559a642d1
Kulesh <tati.kulesh@gmail.com>
2018-05-20 22:21:19 +0300

    lesson4 start

--- src/by/it/tkulesh/lesson05/TaskB2.java
+++ src/by/it/tkulesh/lesson05/TaskB2.java
@@ -20,6 +20,30 @@ b=[11, 22, 33, 44, 55, 66, 77, 88, 99, 0]
 
 */
 
-public class TaskB2 {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.Scanner;
 
-}
+public class TaskB2 {
+    public static void main(String[] args) {
+        ArrayList<Integer> M=new ArrayList(20);
+        Scanner scan=new Scanner(System.in);
+        for (int i = 0; i <=19 ; i++) {
+            M.add(i=scan.nextInt());
+        }
+        ArrayList<Integer> a=new ArrayList(10);
+        ArrayList<Integer> b=new ArrayList(10);
+        for (int i = 0; i < 10; i++) {
+            int s = M.get(i);
+            a.add(s);
+        }
+        for (int i = 10; i < 20; i++) {
+            int s = M.get(i);
+            b.add(s);}
+        System.out.println(a);
+        System.out.println(b);
+        }
+    }
+
