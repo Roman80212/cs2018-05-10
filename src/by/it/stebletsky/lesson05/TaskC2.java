cbc0960d983a5711e12e97f24c0008a595f1b39f
ProstoNiko777 <n_stebletsky@mail.ru>
2018-05-20 13:43:41 +0300

    lesson5 May20

---
+++ src/by/it/stebletsky/lesson05/TaskC2.java
@@ -0,0 +1,14 @@
+package by.it.stebletsky.lesson05;
+
+/* Задачка на сортировку
+Задача: Написать программу, которая вводит с клавиатуры 20 чисел и выводит их в убывающем порядке.
+*/
+
+import java.util.Scanner;
+
+public class TaskC2 {
+
+
+
+    }
+
