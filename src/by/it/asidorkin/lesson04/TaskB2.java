1afa73758f938088a57fdec8f0da08494e0660e5
Алексей <venderser@gmail.com>
2018-05-21 18:04:56 +0300

    new tasks

--- src/by/it/asidorkin/lesson04/TaskB2.java
+++ src/by/it/asidorkin/lesson04/TaskB2.java
@@ -26,6 +26,8 @@ size прочитать c клавиатуры. Числа разделить п
 3. Программа должна выводить таблицу умножения size х size.
  */
 public class TaskB2 {
+
+
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int size = scan.nextInt();
--- src/by/it/asidorkin/lesson04/TaskC3.java
+++ src/by/it/asidorkin/lesson04/TaskC3.java
@@ -22,7 +22,60 @@ package by.it.asidorkin.lesson04;
 ...
 десять умножить на десять равно сто
 */
-
+import java.util.Stack;
 public class TaskC3 {
+public static String digitToString(int i){
+    String res="";
+    if(i<20){
+        switch (i){
+            case 1: return "один";
+            case 2: return "два";
+            case 3: return "три";
+            case 4: return "четыре";
+            case 5: return "пять";
 
+            case 6: return "шесть";
+
+            case 7: return "семь";
+
+            case 8: return "восемь";
+
+            case 9: return "девять";
+
+            case 10: return "десять";
+            case 11: return "одинадцать";
+            case 12: return "двенадцать";
+            case 13: return "тринадцать";
+            case 14: return "четырнадцать";
+            case 15: return "пятнадцать";
+            case 16: return "шестнадцать";
+            case 17: return "семнадцать";
+            case 18: return "восемнадцать";
+            case 19: return "девятнадцать";
+        }
+    }else {if(i<100) {
+        int d = i / 10;
+        String ed=d%10==0?"":" "+digitToString(d%10);
+        switch (d){
+            case 2:return "двадцать"+ed;
+            case 3:return "тридцать"+ed;
+            case 4:return "сорок"+ed;
+            case 5:return "пятьдесят"+ed;
+            case 6:return "шестьдесят"+ed;
+            case 7:return "семдесят"+ed;
+            case 8:return "восемдеесят"+ed;
+            case 9:return "девяносто"+ed;
+        }
+    }
+    }
+    return res;
 }
+    public static void main(String[] args) {
+     for (int i=2; i<=10;i++){
+         for (int j=2; j<=10; j++){
+             //System.out.println(num); вывести через метод
+         }
+        }
+    }
+}
+
