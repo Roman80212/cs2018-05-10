7dd9ab3bcec66473f11659d2f0f8c8cd289b509f
Pavel <pasha.sergeevich97@gmail.com>
2018-05-22 21:31:25 +0300

    Tasks

--- src/by/it/psergeevich/lesson04/TaskA3.java
+++ src/by/it/psergeevich/lesson04/TaskA3.java
@@ -1,4 +1,4 @@
-package by.it.stebletsky.lesson04;
+package by.it.psergeevich.lesson04;
 /*
 Существует ли пара?
 Ввести с клавиатуры три целых числа.
@@ -22,17 +22,24 @@ package by.it.stebletsky.lesson04;
  */
 
 
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class TaskA3 {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int a=scan.nextInt();
-        int b=scan.nextInt();
-        int c=scan.nextInt();
-        if (a==b && b==c)
-            System.out.println(a+" "+b+" "+c);
-        else if (a==b);
-    }
 
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int a=scanner.nextInt();
+        int b=scanner.nextInt();
+        int c=scanner.nextInt();
+        if (a==b && b==c)
+            System.out.println(a+ " "+b+" "+c);
+        else if (a==b)
+            System.out.println(a+" "+b);
+        else if (b==c)
+            System.out.println(b+" "+c);
+        else if (a==c)
+            System.out.println(a+" "+c);
+
+    }
 }
