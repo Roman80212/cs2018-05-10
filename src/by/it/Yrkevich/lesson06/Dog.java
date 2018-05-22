a82382daa30d5e4584baac9c6f32b0d17e9c6f24
Roman80212 <fonarik33@gmail.com>
2018-05-22 21:24:50 +0300

    lesson02

---
+++ src/by/it/Yrkevich/lesson06/Dog.java
@@ -0,0 +1,7 @@
+package by.it.Yrkevich.lesson06;
+
+public class Dog {
+    boolean win (Dog other Dog) {
+        double myChance = 02* this. age+0.3*this.weight+0.5 *this.
+    }
+}
---
+++ src/by/it/Yrkevich/lesson06/DogHelper.java
@@ -0,0 +1,18 @@
+package by.it.Yrkevich.lesson06;
+
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
+public class DogHelper {
+    void printAllNames(Dog[] dogs) {
+        for(Dog dog: dogs){
+            System.out.print(dog);
+
+            for (int i = 0; i < ; i++) {
+            }
+        }
+
+    }
+
+    double averageAge(Dog[] dogs){.....}; //должен вычислять средний возраст переданных ему собак
+
+}
