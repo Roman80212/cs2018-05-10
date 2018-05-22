5624567cb04cab4fb6f713a56d92d8207d2e8f92
vkalenik <uladzimirkalenik@gmail.com>
2018-05-22 20:51:42 +0300

    lesson6 a2

--- src/by/it/vkalenik/lesson06/Dog.java
+++ src/by/it/vkalenik/lesson06/Dog.java
@@ -4,6 +4,14 @@ public class Dog {
     private String name;
     private int age;
 
+    public Dog(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Dog() {
+    }
+
     public String getName() {
         return name;
     }
